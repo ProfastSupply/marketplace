1c0159841e6628bb3488025cc148010b3664fc74
ProfastSupply <167911690+ProfastSupply@users.noreply.github.com>
2026-02-23 11:53:45 -0800

    Update __manifest__.py

--- auto_save_draft/__manifest__.py
+++ auto_save_draft/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Auto Save Sales Orders',
-    'version': '1.0.0',
+    'version': '18.0.1.0.0',
     'category': 'Technical',
     'summary': 'Automatic saving of draft Sale Orders',
     'description': """
